2ade8e1337c49f4ec9a6c61269cca7f5fff38f21
vanshikasharma-webdev <vanshiarma08@gmail.com>
2026-06-07 22:43:24 +0530

    Added Changes

--- script.js
+++ script.js
@@ -5,5 +5,13 @@
         console.log("Console is looking like a wow")
 
         var a = prompt("Enter your number")
+        var isTrue = prompt("Are you sure you want to leave this site or burn your computer")
 
+        if(isTrue){
+            console.log("Computer is blasting")
+        }
+
+        else{
+            console.log("Computer is not blasting")
+        }
         console.log("Your number is " + a)
