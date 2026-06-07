e5815dffa06993d01bc78d25034f6f3060e660c1
vanshikasharma-webdev <vanshiarma08@gmail.com>
2026-06-07 22:35:21 +0530

    Javascript

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
         alert("Hello World");
     
+        console.log("Hey I am a console")
+        console.log("Console is running")
+        console.log("Console is looking like a wow")
+
+        var a = prompt("Enter your number")
+
+        console.log("Your number is " + a)
